3226c234e3049f322fc2bb5fd922fdcae9d9b1c0
Maha234Zaidi <mraaj130@gmail.com>
2025-01-16 18:20:12 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Topheader from "@/app/components/TopHeader";
+import Header from "@/app/components/Header";
+
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Topheader />
+        <Header />
+        {children}
+       
+        <Footer />
+      </body>
     </html>
   );
 }
